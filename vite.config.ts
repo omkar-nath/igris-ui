9869489e8a40a5c31ad518385f759a957089c4c5
omkar-nath <omkarnath8039@gmail.com>
2025-01-25 21:12:13 +0530

    added pigment css

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { glob } from "glob";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { pigment } from "@pigment-css/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
+  plugins: [react(), pigment({}), libInjectCss(), dts({ include: ["lib"] })],
   build: {
     copyPublicDir: false,
     lib: {
